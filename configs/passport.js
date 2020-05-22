08c8f2335ef1320395186ec5801b82fb9deaeeae
Fabio Menozzi <f.menozzi@yahoo.com>
2020-05-21 21:49:21 -0300

    done to deploy

--- configs/passport.js
+++ configs/passport.js
@@ -51,7 +51,7 @@ passport.use(
   new GoogleStrategy({
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:5000/api/auth/google/callback"
+      callbackURL: `${process.env.RCT_API_URL}/auth/google/callback`
     },
     (accessToken, refreshToken, profile, done) => {
      
