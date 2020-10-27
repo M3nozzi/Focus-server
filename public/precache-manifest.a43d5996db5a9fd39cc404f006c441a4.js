118b33c409398b1b985d31517e6ae3696f7f9bf7
Fabio Menozzi <f.menozzi@yahoo.com>
2020-10-27 09:45:51 -0300

    fix: new build

---
+++ public/precache-manifest.a43d5996db5a9fd39cc404f006c441a4.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4553f08c28f05b2d28b4979e8c3be0b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22ca13427a72feb7fa30",
+    "url": "/static/css/2.dda1d026.chunk.css"
+  },
+  {
+    "revision": "c956f518b4ddb9efe736",
+    "url": "/static/css/main.30f31628.chunk.css"
+  },
+  {
+    "revision": "22ca13427a72feb7fa30",
+    "url": "/static/js/2.5bcd03ce.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.5bcd03ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c956f518b4ddb9efe736",
+    "url": "/static/js/main.0d904937.chunk.js"
+  },
+  {
+    "revision": "4b5f058732edeea7134f",
+    "url": "/static/js/runtime-main.6965d6cd.js"
+  },
+  {
+    "revision": "dbfc41b18c6319e3993b64fbcddb75ec",
+    "url": "/static/media/Creativity.dbfc41b1.svg"
+  },
+  {
+    "revision": "5027a20c1ecb2762ed852b532ae8f64a",
+    "url": "/static/media/LearnOnline.5027a20c.svg"
+  },
+  {
+    "revision": "b37ce7e64d698fddb154b82f7434f343",
+    "url": "/static/media/Officeworker.b37ce7e6.svg"
+  },
+  {
+    "revision": "13215ae5a6997632bdd57912962e939e",
+    "url": "/static/media/OnlineShopping_1.13215ae5.svg"
+  },
+  {
+    "revision": "9be404bf413c933679132a60299f555b",
+    "url": "/static/media/SupportTeam.9be404bf.svg"
+  },
+  {
+    "revision": "85ecb5bf38b00db3d04917d5c150d894",
+    "url": "/static/media/close.85ecb5bf.svg"
+  },
+  {
+    "revision": "110e27fdc556e161a0adf6ec8f63c4e2",
+    "url": "/static/media/error404.110e27fd.svg"
+  },
+  {
+    "revision": "b9b211fda85b162e173bd869d12ab49b",
+    "url": "/static/media/google.b9b211fd.png"
+  },
+  {
+    "revision": "9dde130753b1186581d6af96b777b1a2",
+    "url": "/static/media/magnifying-glass.9dde1307.svg"
+  }
+]);
